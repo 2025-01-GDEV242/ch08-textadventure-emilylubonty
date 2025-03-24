aaf9145ee705824bb190728732738c3ac0c7b832
Emily <159160889+emilylubonty@users.noreply.github.com>
2025-03-24 11:39:38 -0400

    Created new rooms
    created new rooms

--- Game.java
+++ Game.java
@@ -34,7 +34,7 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room outside, theater, pub, lab, office, cafeteria, confrence, tunnel;
       
         // create the rooms
         outside = new Room("outside the main entrance of the university");
@@ -42,6 +42,10 @@ public class Game
         pub = new Room("in the campus pub");
         lab = new Room("in a computing lab");
         office = new Room("in the computing admin office");
+        cafeteria = new Room ("in the campus cafeteria"); 
+        confrence = new Room ("in the confrence room"); 
+        tunnel = new Room ("in the campus' secret tunnel"); 
+        
         
         // initialise room exits
         outside.setExit("east", theater);
